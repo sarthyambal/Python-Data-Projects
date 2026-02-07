274b83e3de293f82d4bef11a0aaca0f25d0189ad
Sarth Milind Yambal <sarthyambal1952000@gmail.com>
2026-02-07 23:22:17 +0530

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,32 @@
+# s1 = {1, 2, 5, 7, 9, 8, 12, 12}
+
+# s2 = set() #This is empty set
+
+# print(s1)
+# items = ("apple", "banana",)
+
+# items.add("orange")
+# print(items)
+
+# items.update(["mango", "peach"])
+# print(items)
+# print(items)
+# items.remove("banana")
+# pritn(items)
+# items.discard("bananaw") # its give no error
+# print(items)
+
+# items.pop()
+# print(items)
+# items.clear()
+# print(items)
+
+
+
+
+a = {1, 2, 4}
+b = {3, 4, 5}
+
+# result = a.union(b)
+result = a.intersection(b)
+print(result)
