9b0aef75d921f0d6c5ad1f7aeef9ba33d55f45fc
Sarth Milind Yambal <sarthyambal1952000@gmail.com>
2026-02-07 23:42:28 +0530

    Add files via upload

---
+++ dictionaries.py
@@ -0,0 +1,26 @@
+student = {
+    "name": "sarth",
+    "city": "pusad",
+    "work": "meta"
+}
+
+# print(student["work"])
+# print(student["city"])
+# # print(student["nameee"]) #it puts an error
+
+# print(student.get("nameeee"))
+
+# student["city"] = "Yavtmal"
+# print(student)
+
+# student.pop("name")
+# print(student)
+
+# student["class"] = "12th"
+# print(student)
+# student.popitem()
+# print(student)
+
+# student.clear()
+print(student.keys())
+print(student)
