29ea72fb085f62b7d2d9f7b0620ba241ce6db443
Sarth Milind Yambal <sarthyambal1952000@gmail.com>
2026-02-08 17:07:26 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,33 @@
+items = ["apples" , 34, "bananas"]
+
+for items in items:
+    print(items)
+
+for i in range(0, 10, 2):
+    print(i)
+
+for char in "Sarth":
+    print(char)
+
+count = 1
+
+while (count <=5):
+    print(count)
+    count += 1
+
+print("End of programe")
+
+for i in range(0, 10):
+    if(i==5):
+        break
+    print(i)
+
+print("End Of Loop")    
+
+
+for i in range(0, 10):
+    if(i==5):
+       continue
+    print(i)
+
+print("End Of Loop")    
