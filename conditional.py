eaf2b12eabadc463c65267746e0592220ddbc3d7
Sarth Milind Yambal <sarthyambal1952000@gmail.com>
2026-02-08 16:46:02 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,33 @@
+a = int(input("Enter The Number: "))
+
+if(a > 40): 
+    print("a is greather than 40")
+    print("Thank You")
+
+else: 
+    print("a is not greather than 40") 
+
+age = 16
+if age >= 18:
+    print("You can drive")
+else:
+    print("You cannot drive")    
+
+
+Marks = 75
+
+if Marks >= 80:
+    print("Grade A")
+elif Marks >= 60:
+    print("Grade B")
+else:
+    print("Grade C")        
+
+if Marks % 2 == 0:
+    print("Marks is odd")
+else:
+    print("Marks is Even")    
+
+age = 20 
+
+status = "Adult" if age >= 18 else "Minor"
