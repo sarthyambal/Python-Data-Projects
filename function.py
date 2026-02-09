7aaf2eca323c719f669c2faed65ef3f3ffeecadc
Sarth Milind Yambal <sarthyambal1952000@gmail.com>
2026-02-09 22:01:00 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,24 @@
+# def greet(fname, lname):
+#     print("Good morning", fname, lname)
+#     print("How are you")
+#     print("Thank you!")
+
+# greet("Sarth", "Yambal")
+# greet("Prem", "Sadawarte")
+
+# def add(a,b):
+     # print(a+b)
+#     return(a + b)
+# c = add(100,2000)
+# print(c)
+
+
+# def greet(name = "User", city="Dahegao"):
+#     print("Hello",name , city)
+
+# greet()
+# greet("Prem")
+# greet(city="Dahegao", name="Prem")
+
+add = lambda a,b: a+b
+print(add(10000,10000))
