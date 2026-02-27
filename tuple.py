aa5a8e67853ee4b5c9d8d50df72bc27e4dc131e5
Sarth Milind Yambal <sarthyambal1952000@gmail.com>
2026-02-27 23:33:27 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,16 @@
+# numbers = (1, 12, 34, 56)
+
+# print(numbers[0])
+# print(numbers[1])
+# print(numbers[2])
+
+# single = (1,) # Single Element Tuple
+
+items = ("Apple", "Banana", "Orange")
+
+print(items[-1])
+print(len(items))
+
+print(items.index("Banana"))
+list_1 = list(items)
+print(list_1)
