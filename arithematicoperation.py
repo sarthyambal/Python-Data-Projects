b0909f22d83b7652c0ee3ffe0981949d69ca68b0
Sarth Milind Yambal <sarthyambal1952000@gmail.com>
2026-05-12 16:11:57 +0530

    Add files via upload

---
+++ arithematicoperation.py
@@ -0,0 +1,10 @@
+x, y=15, 4
+
+print('---Arithematic Operation----')
+print('Addition:', x + y)
+print('Subtraction:', x - y)
+print('Multiplication:', x * y)
+print('division:', x / y)
+print('FloorDivision:', x // y)
+print('Modulus:', x % y)
+print('Power:', x ** y)
