0ba8734f81bb6244ae127df9304aecda2a28c4ed
Sarth Milind Yambal <sarthyambal1952000@gmail.com>
2026-05-12 16:12:41 +0530

    Add files via upload

---
+++ Lists and Collections.py
@@ -0,0 +1,30 @@
+# lst = [10,20,40]
+# lst.append(40)
+# lst.insert(1,15)
+# lst.remove(20)
+# popped = lst.pop()
+# print("List:", lst)
+# print("popped:", popped)
+
+# numbers = [5, 3, 8, 1, 9, 2]
+# print("\nMax", max(numbers))
+# print("Min", min(numbers))
+# print("Sum", sum(numbers))
+
+# import numbers
+# numbers.sort()
+# print("ascending:", numbers)
+# numbers.sort(reverse=True)
+# print("descending:", numbers)
+
+students = {"SARTH": 92, "RAHUL": 74, "PRIYA":88,}
+# print("\n----Students Dictionary----")
+# print(students)
+
+# print("\nName - Marks")
+# for name, marks in students.items():
+#     print(f"{name} - {marks}")
+
+students['RAHUL', 85]
+print("\nAfter updating Rahul's marks:", students)
+print(students)
